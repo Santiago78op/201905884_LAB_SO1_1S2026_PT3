cc68240fc830a6d5343c082637e4a38b484660ee
Santiago78op <2993696170101@ingenieria.usac.edu.gt>
2026-04-17 22:11:55 -0400

    Create a server struct  for new server

--- go-grpc-server-writer/cmd/app/main.go
+++ go-grpc-server-writer/cmd/app/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"201905884_LAB_SO1_1S2026_PT3/go-grpc-server-writer/internal/server"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	// Conexion a RabbitMQ
-	conn, err := amqp.Dial()
+	// Coneccion a RabbitMQ
+	conn, err := amqp.Dial("amqp://guest:guest@locatehost:5672/")
+	// Si falla importamos Panicf con err
+	failMsg := server.OutPutFaildOnError{}
+	failMsg.FaildOnError(err, "Fail to connect to RabbitMQ")
+	defer conn.Close()
+
+	// Abrir canal
+	ch, err := conn.Channel()
+	failMsg.FaildOnError(err, "Faild to open a channel")
+	defer ch.Close()
+
+	// Creación de server Struct
+	srv := server.NewServer(conn, ch)
+
 }
--- go-grpc-server-writer/internal/server/grpc-server.go
+++ go-grpc-server-writer/internal/server/grpc-server.go
@@ -1,46 +1,34 @@
 package server
 
 import (
-	"encoding/json"
 	"context"
+	"encoding/json"
+	"log"
 	"time"
 
 	proto "201905884_LAB_SO1_1S2026_PT3/proto"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-/* Server struct 
-* Hace referencia a la conexión y canal de RabbitMQ
-* connection: es la conexión física con el servidor RabbitMQ
-* channel: es el canal virtual que vive dentro de esa conexión
-* UnimplementedWarReportServiceServer: es el struct auxiliar 
-* 	generado por protoc para implementar la interfaz WarReportServiceServer
-*/
+type OutPutFaildOnError struct{}
+
 type Server struct {
 	connection *amqp.Connection
 	channel    *amqp.Channel
-	proto.UnimplementedWarReportServiceServer // Embed para satisfacer la interfaz
+	proto.UnimplementedWarReportServiceServer
 }
 
-/* Metodo de Negocio SendReport 
-* Encargado de procesar el reporte militar y publicarlo en RabbitMQ
-* Recibe un WarReportRequest y devuelve un WarReportResponse
-* Serializa el request y publica en RabbitMQ
-* Publicar JSON en RabbitMQ con el channel
-* Retorna un WarReportResponse con el status "ok"
-*/
-func (s *Server) SendReport(ctx context.Context, req *proto.WarReportRequest)(*proto.WarReportResponse, error) {
-	// json Marshall
+func (s *Server) SendReport(ctx context.Context, req *proto.WarReportRequest) (*proto.WarReportResponse, error) {
 	jsonData, err := json.Marshal(req)
 
-	// Si hay error, fileOnError
-	faildOnError(err, "Fail to serealize request")
+	o := OutPutFaildOnError{}
+	o.FaildOnError(err, "Fail to serealize request")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	// Publicar en RabbitMQ con el channel
-	err = s.channel.PublishWithContext({ctx,
+	err = s.channel.PublishWithContext(ctx,
 		"warreport_exchange",
 		"warreport.process",
 		false,
@@ -49,16 +37,26 @@ func (s *Server) SendReport(ctx context.Context, req *proto.WarReportRequest)(*p
 			ContentType: "application/json",
 			Body:        jsonData,
 		},
-	})
+	)
 
-	failedToPublish(err, "Failed to publish to RabbitMQ")
+	o.FaildOnError(err, "Failed to publish to RabbitMQ")
 
-	// Retornar respuesta exitosa
 	return &proto.WarReportResponse{Status: "ok"}, nil
 }
 
-func failedToPublish(err error, message string) {
+// Función para manejar errores
+func (o OutPutFaildOnError) FaildOnError(err error, msg string) {
 	if err != nil {
-		log.Fatalf("%s: %v", message, err)
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+/* NewServer Struct
+* Función para crear un nuevo servidor gRPC con conexión a RabbitMQ
+ */
+func NewServer(conn *amqp.Connection, ch *amqp.Channel) *Server {
+	return &Server{
+		connection: conn,
+		channel:    ch,
 	}
 }
